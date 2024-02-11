8a526b3508c9b8f2fab37641f04fc40ab9abbd33
Javier Torres Rivera <javier.torres16@upr.edu>
2024-02-10 21:06:48 -0400

    TEst

---
+++ backend/controller/person.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+
+
+from backend.dao.person import personDAO
+
+
+
+class PersonController:
+
+    def build_dictionary(self,row):
+        result={}
+        result['id']=row[0]
+        result['Name']=row[1]
+        result['LastName']=row[2]
+        return result
+    
+
+
+
+    def getAll(self):
+        dao= personDAO()
+        result_tuples= dao.getAll()
+        result=[]
+        for row in result_tuples:
+            dictionary=self.build_dictionary(row)
+            result.append(dictionary)
+        return jsonify
