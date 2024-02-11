fd1887ea069744af677d89ca710064e938a8bfb0
Javier Torres Rivera <javier.torres16@upr.edu>
2024-02-10 23:30:32 -0400

    Fresh Start

--- backend/config/pg_config.py
+++
@@ -1,8 +0,0 @@
-pg={
-
-    'HOST':'aws-0-us-east-1.pooler.supabase.com',
-    'DATABASE':'postgres',
-    'USER':'postgres.cveicdaizlxtslvosxqm',
-    'PASS':'gYPndVdJQlupiJoS',
-    'PORT':'5432'
-}
